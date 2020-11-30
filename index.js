e957d5f92a749fab04944119c832fa51c195dc7b
matthewxvoorhees <matthew.crnich@gmail.com>
2020-11-30 14:16:52 -0700

    Done.

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(function(sentence){
+    console.log(sentence.split(' ').map(word =>  word[0].toUpperCase() + word.slice(1)).join(' '))
+    return sentence.split(' ').map(word => word[0].toUpperCase() + word.slice(1)).join(' ')
+  })
 }
